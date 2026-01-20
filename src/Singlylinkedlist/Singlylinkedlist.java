a62913732edc0b656147662da3a4cb4b257ff9ad
maram2236moad <marammoadalazazy227@gmail.com>
2026-01-20 18:40:37 +0300

    v1 array

--- src/Singlylinkedlist/Singlylinkedlist.java
+++ src/Singlylinkedlist/Singlylinkedlist.java
@@ -29,7 +29,7 @@ public class Singlylinkedlist<E> {
         public E getData() {
             return data;
         }
-
+ 
         public void setData(E data) {
             this.data = data;
         }
